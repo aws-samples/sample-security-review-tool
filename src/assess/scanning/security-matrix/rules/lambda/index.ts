f5a88214d7d2f34dbad9462e58691be6d3e5e1b4
Brad Webber <bradweb@amazon.com>
2026-05-21 20:39:12 +0000

    Removed rules

--- src/assess/scanning/security-matrix/rules/lambda/index.ts
+++ src/assess/scanning/security-matrix/rules/lambda/index.ts
@@ -1,20 +1,17 @@
 import rule002 from './002-secrets-manager.cf.js';
 import rule005 from './005-least-privilege-roles.cf.js';
-import rule011 from './011-cloudwatch-alarms.cf.js';
 import rule012 from './012-unique-execution-role.cf.js';
 import rule015 from './015-container-image-repository.cf.js';
 
 export const lambdaRules = [
   rule002,
   rule005,
-  rule011,
   rule012,
   rule015
 ];
 
 import tfRule001 from './002-secrets-manager.tf.js';
 import tfRule004 from './005-least-privilege-roles.tf.js';
-import tfRule005 from './011-cloudwatch-alarms.tf.js';
 import tfRule006 from './012-unique-execution-role.tf.js';
 import tfRule007 from './013-secrets-in-env-vars.tf.js';
 import tfRule008 from './015-container-image-repository.tf.js';
@@ -23,7 +20,6 @@ import tfRule009 from './016-container-image-scanning.tf.js';
 export const tfLambdaRules = [
   tfRule001,
   tfRule004,
-  tfRule005,
   tfRule006,
   tfRule007,
   tfRule008,
