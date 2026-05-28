c12e7f10c0baa9c4062800083da09a5c614acf45
Brad Webber <bradweb@amazon.com>
2026-05-28 15:06:45 +0000

    Removed old rules

--- src/assess/scanning/security-matrix/rules/cloudfront/index.ts
+++ src/assess/scanning/security-matrix/rules/cloudfront/index.ts
@@ -1,12 +1,10 @@
 // rule001 (CFR1 - Geo restrictions) is not implemented as determining which geographies should be allowed or blocked requires understanding the business use case, which cannot be inferred automatically
-import rule002 from './002-waf-protection.cf.js';
 import rule003 from './003-access-logging.cf.js';
 import rule004 from './004-https-only.cf.js';
 import rule005 from './005-origin-https.cf.js';
 import rule006 from './006-origin-access-control.cf.js';
 
 export const cloudfrontRules = [
-  rule002,
   rule003,
   rule004,
   rule005,
@@ -14,21 +12,18 @@ export const cloudfrontRules = [
 ];
 
 export {
-  rule002 as wafProtectionRule,
   rule003 as accessLoggingRule,
   rule004 as httpsOnlyRule,
   rule005 as originHttpsRule,
   rule006 as originAccessControlRule,
 };
 
-import tfRule001 from './002-waf-protection.tf.js';
 import tfRule002 from './003-access-logging.tf.js';
 import tfRule003 from './004-https-only.tf.js';
 import tfRule004 from './005-origin-https.tf.js';
 import tfRule005 from './006-origin-access-control.tf.js';
 
 export const tfCloudfrontRules = [
-  tfRule001,
   tfRule002,
   tfRule003,
   tfRule004,
