07fb8c1c07ca127417d2b4c2bf8365d0364a66cf
Brad Webber <bradweb@amazon.com>
2026-06-01 13:03:15 +0000

    Removed old CFR-004 rule

--- src/assess/scanning/security-matrix/rules/cloudfront/index.ts
+++ src/assess/scanning/security-matrix/rules/cloudfront/index.ts
@@ -1,21 +1,16 @@
 // rule001 (CFR1 - Geo restrictions) is not implemented as determining which geographies should be allowed or blocked requires understanding the business use case, which cannot be inferred automatically
-import rule004 from './004-https-only.cf.js';
 import rule005 from './005-origin-https.cf.js';
 
 export const cloudfrontRules = [
-  rule004,
   rule005,
 ];
 
 export {
-  rule004 as httpsOnlyRule,
   rule005 as originHttpsRule,
 };
 
-import tfRule003 from './004-https-only.tf.js';
 import tfRule004 from './005-origin-https.tf.js';
 
 export const tfCloudfrontRules = [
-  tfRule003,
   tfRule004,
 ];
