171e8b4f00a96aead612799dc0c70de9722640ac
Brad Webber <bradweb@amazon.com>
2026-08-10 17:09:37 +0000

    Finalized API-GW-001 migration

--- src/assess/scanning/security-matrix/rules/api-gateway/index.ts
+++ src/assess/scanning/security-matrix/rules/api-gateway/index.ts
@@ -1,4 +1,3 @@
-import rule001 from './001-access-logging.cf.js';
 import rule002 from './002-request-validation.cf.js';
 import rule003 from './003-waf-protection.cf.js';
 import rule004 from './004-authentication.cf.js';
@@ -10,7 +9,6 @@ import rule009 from './009-private-endpoints.cf.js';
 // rule010 (APIG10 - Sensitive info logging) is not implemented as it's impossible to fully detect automatically because the content of logged parameters depends on runtime API inputs
 
 export const apiGatewayRules = [
-  rule001,
   rule002,
   rule003,
   rule004,
@@ -22,7 +20,6 @@ export const apiGatewayRules = [
 ];
 
 export {
-  rule001 as accessLoggingRule,
   rule002 as requestValidationRule,
   rule003 as wafProtectionRule,
   rule004 as authenticationRule,
@@ -33,7 +30,6 @@ export {
   rule009 as privateEndpointsRule
 };
 
-import tfRule001 from './001-access-logging.tf.js';
 import tfRule002 from './002-request-validation.tf.js';
 import tfRule003 from './003-waf-protection.tf.js';
 import tfRule004 from './004-authentication.tf.js';
@@ -44,7 +40,6 @@ import tfRule008 from './008-cache-encryption.tf.js';
 import tfRule009 from './009-private-endpoints.tf.js';
 
 export const tfApiGatewayRules = [
-  tfRule001,
   tfRule002,
   tfRule003,
   tfRule004,
