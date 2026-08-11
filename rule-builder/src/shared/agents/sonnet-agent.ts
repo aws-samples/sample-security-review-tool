15af40cf2131f36689d749a7ab49c33c745013aa
Brad Webber <bradweb@amazon.com>
2026-08-11 19:02:09 +0000

    Increased max tokens for models

--- rule-builder/src/shared/agents/sonnet-agent.ts
+++ rule-builder/src/shared/agents/sonnet-agent.ts
@@ -3,7 +3,7 @@ import { Agent, AgentConfig, BedrockModel } from "@strands-agents/sdk";
 export class SonnetAgent extends Agent {
     constructor(config?: AgentConfig | undefined) {
         config = config ?? {};
-        config.model = new BedrockModel({ modelId: 'global.anthropic.claude-sonnet-5' });
+        config.model = new BedrockModel({ modelId: 'global.anthropic.claude-sonnet-5', maxTokens: 64000 });
         super(config);
     }
 }
