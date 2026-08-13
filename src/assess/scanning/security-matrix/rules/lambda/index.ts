b9949ba19ea9c76a3c8bad219f3c89fb12a6d79d
Brad Webber <bradweb@amazon.com>
2026-08-13 15:51:10 +0000

    Updated lambda rules

--- src/assess/scanning/security-matrix/rules/lambda/index.ts
+++ src/assess/scanning/security-matrix/rules/lambda/index.ts
@@ -1,15 +1,11 @@
-import rule002 from './002-secrets-manager.cf.js';
 import rule005 from './005-least-privilege-roles.cf.js';
 
 export const lambdaRules = [
-  rule002,
   rule005,
 ];
 
-import tfRule001 from './002-secrets-manager.tf.js';
 import tfRule004 from './005-least-privilege-roles.tf.js';
 
 export const tfLambdaRules = [
-  tfRule001,
   tfRule004,
 ];
