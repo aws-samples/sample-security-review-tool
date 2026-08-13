e27bbbda01c3f0be3765a5a6fefb947629671c60
Brad Webber <bradweb@amazon.com>
2026-08-13 15:22:43 +0000

    Removed duplicate rule

--- src/assess/scanning/security-matrix/rules/api-gateway/index.ts
+++ src/assess/scanning/security-matrix/rules/api-gateway/index.ts
@@ -1,21 +1,16 @@
 import rule008 from './008-cache-encryption.cf.js';
-import rule009 from './009-private-endpoints.cf.js';
 // rule010 (APIG10 - Sensitive info logging) is not implemented as it's impossible to fully detect automatically because the content of logged parameters depends on runtime API inputs
 
 export const apiGatewayRules = [
   rule008,
-  rule009
 ];
 
 export {
   rule008 as cacheEncryptionRule,
-  rule009 as privateEndpointsRule
 };
 
 import tfRule008 from './008-cache-encryption.tf.js';
-import tfRule009 from './009-private-endpoints.tf.js';
 
 export const tfApiGatewayRules = [
   tfRule008,
-  tfRule009,
 ];
